1b8aaa36ff02fb41d9742c059886dad8e3e09cff
João Vitor Fonseca da Silva <joaovfonseca@outlook.com>
2022-01-04 14:26:08 -0300

    Alterações

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -7,10 +7,19 @@ import { Component } from "@angular/core";
 })
 
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number;
   destino: number;
+
   transferir() {
-    console.log(this.valor, this.destino);
-    console.log('Transferindo...');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = null;
+    this.destino = null;
   }
 }
